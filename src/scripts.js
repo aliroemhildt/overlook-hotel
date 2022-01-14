dcd319b48efd7d84e75982ebdd9e1bea6f6c7a2d
Ali Roemhildt <aliroemhildt@gmail.com>
2022-01-14 11:23:11 -0700

    Create css file and add background

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,7 @@
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
 import {
   fetchCustomers,
