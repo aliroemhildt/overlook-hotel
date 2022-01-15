fe214b2a3d0190ffa4404e0872144687769e726d
Ali Roemhildt <aliroemhildt@gmail.com>
2022-01-14 18:08:09 -0700

    Add data files

---
+++ src/data/bookings-data.js
@@ -0,0 +1,60 @@
+const sampleBookings = [
+  {
+  "id": "5fwrgu4i7k55hl6sz",
+  "userID": 9,
+  "date": "2022/04/22",
+  "roomNumber": 15,
+  "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t5",
+    "userID": 43,
+    "date": "2022/01/24",
+    "roomNumber": 24,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t6",
+    "userID": 13,
+    "date": "2022/01/10",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t7",
+    "userID": 20,
+    "date": "2022/02/16",
+    "roomNumber": 7,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t8",
+    "userID": 1,
+    "date": "2022/02/05",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t9",
+    "userID": 38,
+    "date": "2022/02/14",
+    "roomNumber": 14,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6ta",
+    "userID": 25,
+    "date": "2022/01/11",
+    "roomNumber": 9,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6tq",
+    "userID": 1,
+    "date": "2022/03/05",
+    "roomNumber": 45,
+    "roomServiceCharges": []
+  },
+];
+
+export default sampleBookings;
---
+++ src/data/customers-data.js
@@ -0,0 +1,12 @@
+const sampleCustomers = [
+  {
+  "id": 1,
+  "name": "Leatha Ullrich"
+  },
+  {
+  "id": 2,
+  "name": "Rocio Schuster"
+  }
+];
+
+export default sampleCustomers;
